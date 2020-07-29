f3de8a78646ad52730c4e0e795a65035ce082a14
nikhil.tangella <nikhil.tangella@servicenow.com>
2020-07-29 19:16:33 -0400

    some changes to event

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Event = (props) => {
     return (
+        <div>
         <div>Event</div>
+        <div>{props.event.name}</div>
+        <div>{props.event.date}</div>
+        <div>{props.event.location}</div>
+        <button>Completed</button>
+        </div>
     );
 }
 export default Event
