68bb5336664e50da3403e9b93590d15f2bc0b81e
jaden.padua <jaden.padua@servicenow.com>
2020-07-29 14:52:10 -0700

    Create event sections and event component framework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import cat from "./assets/cat.gif";
 import "./App.css";
+import Event from "./components/Event";
 
 function App() {
   return (
-       <div className="App">
-           <h1>Welcome to Game-Now</h1>
-               <div className="cat">
-                  <img src={cat} alt="a cat" />
-            </div>
+    <div className="App">
+      <h1>Welcome to Game-Now!</h1>
+      <section className="upcoming-events">
+        <h2> Upcoming Events</h2>
+        <Event />
+      </section>
+      <section className="completed-events">
+      <h2> Completed Events</h2>
+        <Event />
+      </section>
+      <section className="past-events">
+      <h2> Past Events</h2>
+        <Event />
+      </section>
     </div>
   );
 }
