e3ef5dd24dab051b562751f7e3284407b42cdd9d
jaden.padua <jaden.padua@servicenow.com>
2020-07-29 15:46:02 -0700

    Fix dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Event from "./components/Event";
-import {events} from "./utils"
+import { events } from "./utils";
 
 const App = () => {
   return (
@@ -9,18 +9,19 @@ const App = () => {
       <h1>Welcome to Game-Now!</h1>
       <section className="upcoming-events">
         <h2> Upcoming Events</h2>
-        <Event name={events.event1[0]} date={events.event1[1]} location={events.event1[2]}/>
+
+        <Event event={events[0]}/>
       </section>
       <section className="completed-events">
         <h2> Completed Events</h2>
-        <Event name={events.event1[0]} date={events.event1[1]} location={events.event1[2]}/>
+        <Event event={events[0]}/>
       </section>
       <section className="past-events">
         <h2> Past Events</h2>
-        <Event name={events.event1[0]} date={events.event1[1]} location={events.event1[2]}/>
+        <Event event={events[0]}/>
       </section>
     </div>
   );
-}
+};
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,27 @@
-export const events = {
-    event1: ["Event 1", "Aug 6", "https://servicenow.zoom.us/j/94445515273"],
-    event2: ["Event 2", "Aug 19", "https://servicenow.zoom.us/j/94445515273"],
-    event3: ["Event 3", "Sept 21", "https://servicenow.zoom.us/j/94445515273"],
-    event4: ["Event 4", "Nov 7", "https://servicenow.zoom.us/j/94445515273"],
-    event5: ["Event 5", "Dec 22", "https://servicenow.zoom.us/j/94445515273"]
-}
+const event1 = {
+  name: "Event1",
+  date: "Aug 6",
+  location: "https://servicenow.zoom.us/j/94445515273",
+};
+const event2 = {
+  name: "Event2",
+  date: "Aug 21",
+  location: "https://servicenow.zoom.us/j/94445515273",
+};
+const event3 = {
+  name: "Event3",
+  date: "Sep 9",
+  location: "https://servicenow.zoom.us/j/94445515273",
+};
+const event4 = {
+  name: "Event4",
+  date: "Sep 22",
+  location: "https://servicenow.zoom.us/j/94445515273",
+};
+const event5 = {
+  name: "Event5",
+  date: "Dec 18",
+  location: "https://servicenow.zoom.us/j/94445515273",
+};
+
+export const events = [event1, event2, event3, event4, event5];
