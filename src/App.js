48b85e160e5aa32708cacef895571ec249a9230d
jaden.padua <jaden.padua@servicenow.com>
2020-07-29 11:13:21 -0700

    cat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import cat from "./assets/cat.gif";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="App">
+           <h1>Welcome to Game-Now</h1>
+               <div className="cat">
+                  <img src={cat} alt="a cat" />
+            </div>
     </div>
   );
 }
