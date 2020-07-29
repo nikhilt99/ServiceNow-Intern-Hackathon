80a4896974a3172702e60170fbaba5a502cbc759
jaden.padua <jaden.padua@servicenow.com>
2020-07-29 15:21:45 -0700

    Dummy data and pass into props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./App.css";
 import Event from "./components/Event";
+import {events} from "./utils"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Welcome to Game-Now!</h1>
       <section className="upcoming-events">
         <h2> Upcoming Events</h2>
-        <Event />
+        <Event name={events.event1[0]} date={events.event1[1]} location={events.event1[2]}/>
       </section>
       <section className="completed-events">
-      <h2> Completed Events</h2>
-        <Event />
+        <h2> Completed Events</h2>
+        <Event name={events.event1[0]} date={events.event1[1]} location={events.event1[2]}/>
       </section>
       <section className="past-events">
-      <h2> Past Events</h2>
-        <Event />
+        <h2> Past Events</h2>
+        <Event name={events.event1[0]} date={events.event1[1]} location={events.event1[2]}/>
       </section>
     </div>
   );
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export const events = {
+    event1: ["Event 1", "Aug 6", "https://servicenow.zoom.us/j/94445515273"],
+    event2: ["Event 2", "Aug 19", "https://servicenow.zoom.us/j/94445515273"],
+    event3: ["Event 3", "Sept 21", "https://servicenow.zoom.us/j/94445515273"],
+    event4: ["Event 4", "Nov 7", "https://servicenow.zoom.us/j/94445515273"],
+    event5: ["Event 5", "Dec 22", "https://servicenow.zoom.us/j/94445515273"]
+}
