8c066923ad2324c6c78c591d58f68add00e765e4
jaden.padua <jaden.padua@servicenow.com>
2020-07-29 16:22:16 -0700

    minor style refactoring

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Event = (props) => {
     return (
         <div>
-        <div>Event</div>
         <div>{props.event.name}</div>
         <div>{props.event.date}</div>
         <div>{props.event.location}</div>
