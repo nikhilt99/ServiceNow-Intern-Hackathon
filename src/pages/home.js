0fdbbacd5c91f6f7bc18d3a32384f5542d392b40
Jeffrey <jc2573@cornell.edu>
2020-07-30 16:51:40 -0700

    Bring back home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,39 @@
 import React from "react";
 import Dashboard from "../components/Dashboard/Dashboard";
+import {
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
+import EventCard from "../components/EventCard/EventCard";
 
 const Home = () => {
   return (
-    <Dashboard name="Jeffrey Chou" points="375" level="2" teammates="Kenneth Chen, Nikhil Tangella, Jaden Padua"></Dashboard>
+    <div>
+      <Dashboard name="Jeffrey Chou" points="375" level="2" teammates="Kenneth Chen, Nikhil Tangella, Jaden Padua"></Dashboard>
+      <h1>My Events</h1>
+      <Row className="my-event-tabs">
+        <Col md="auto">
+          <p className="active">Upcoming</p>
+        </Col>
+        <Col md="auto">
+          <p>Past</p>
+        </Col>
+        <Col md="auto">
+          <p>All</p>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <EventCard />
+        </Col>
+      </Row>
+    </div>
   );
 }
 
